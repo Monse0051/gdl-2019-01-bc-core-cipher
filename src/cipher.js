2a7591997e78d2243d2281416e354c1aca2a8c37
Monse0051 <rouge0051@gmail.com>
2019-01-24 20:51:50 -0600

    Plantilla del cipher y pequeño codigo para hacer pruebas.

--- src/cipher.js
+++ src/cipher.js
@@ -1,4 +1,22 @@
 window.cipher = {
-  // TODO: add method encode(offset, string)
-  // TODO: add method decode(offset, string)
+
+  encode(offset, string){
+    console.log("calling encode");
+    return "fsdjkhf";
+  },
+
+  decode(offset, string){
+    console.log("calling decode");
+
+    return "jhfkjd";
+  }
+
 };
+
+// playground
+let msgOriginal = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let msgEncoded = window.cipher.encode(3, msgOriginal);
+console.log("mensaje cifrado = " + msgEncoded);
+let msgDecoded = window.cipher.decode(3, msgEncoded);
+
+console.log("mensaje original" + msgDecoded);
