be316deaf01381fb852ccc132c02be12162c38d8
Monse0051 <rouge0051@gmail.com>
2019-01-29 22:09:31 -0600

    Complete las funciones de cifrar y descifrar

--- src/cipher.js
+++ src/cipher.js
@@ -1,22 +1,43 @@
 window.cipher = {
 
-  encode(offset, string){
-    console.log("calling encode");
-    return "fsdjkhf";
+   encode(offset, msgOriginal) {
+    let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const alphabetSize = alphabet.length;
+    let msgEncode = "";
+    for (let i= 0; i<= msgOriginal.length-1; i++) {
+      if (msgOriginal[i] == " ") {
+          msgEncode += " ";
+       } else {
+         let pos = (msgOriginal.charCodeAt(i) - "A".charCodeAt(0) + offset) % alphabetSize;
+         msgEncode += alphabet[pos];
+       }
+    }
+
+    return msgEncode;
   },
 
-  decode(offset, string){
-    console.log("calling decode");
-
-    return "jhfkjd";
-  }
-
+    decode(offset,  msgCodificado) {
+     let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+     const alphabetSize = alphabet.length;
+     let msgDecoded = "";
+     for (let i= 0; i<=  msgCodificado.length-1; i++) {
+       if (msgCodificado[i] == " ") {
+           msgDecoded += " ";
+        } else {
+          let pos = (msgCodificado.charCodeAt(i) + "A".charCodeAt(0) - offset) % alphabetSize;
+          msgDecoded += alphabet[pos];
+         }
+     }
+     return msgDecoded;
+   }
 };
 
+
 // playground
-let msgOriginal = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let msgOriginal = "HOLA MUNDO CRUEL";
 let msgEncoded = window.cipher.encode(3, msgOriginal);
 console.log("mensaje cifrado = " + msgEncoded);
+
 let msgDecoded = window.cipher.decode(3, msgEncoded);
 
-console.log("mensaje original" + msgDecoded);
+console.log("mensaje original " + msgDecoded);
